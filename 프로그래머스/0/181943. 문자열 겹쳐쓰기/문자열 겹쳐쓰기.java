3d0f5274f54e72f2bee376290cd4e01aea4c2698
chaewon <150315730+cochae@users.noreply.github.com>
2025-09-12 12:54:25 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder();
+        String part1 = my_string.substring(0, s);
+        sb.append(part1);
+        sb.append(overwrite_string);
+        String part2 = my_string.substring(s+overwrite_string.length());
+        sb.append(part2);
+        String answer = sb.toString();
+        return answer;
+    }
+}
