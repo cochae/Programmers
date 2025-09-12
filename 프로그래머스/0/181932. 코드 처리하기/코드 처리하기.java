58f232a0c6e0a748a57a4fe704d50f79e53e5a8e
chaewon <150315730+cochae@users.noreply.github.com>
2025-09-12 12:57:23 +0900

    [level 0] Title: 코드 처리하기, Time: 9.76 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String code) {
+        int mode = 0;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+
+            if (c == '1') {
+                mode = 1-mode;
+                continue;
+            }
+
+            if (i % 2 == mode) {
+                answer.append(c);
+            }
+        }
+
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
+    }
+}
