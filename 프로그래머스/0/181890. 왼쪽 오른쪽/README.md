ae57ed595f93725b7562a256553a81c22c7cd47a
chaewon <150315730+cochae@users.noreply.github.com>
2025-09-25 16:17:36 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.14 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890#) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 0.04 ms
+메모리: 70.7 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 25일 16:05:29
+2025년 09월 25일 16:17:33
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,24 +2,14 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] str_list) {
         String[] answer = {};
-        int index_l = -1;
-        int index_r = -1;
         for(int i = 0; i < str_list.length; i++){
             if(str_list[i].equals("l")) {
-                index_l = i;
-                break;
+                return Arrays.copyOf(str_list, i);
             }
             else if(str_list[i].equals("r")){
-                index_r = i;
-                break;
+                return Arrays.copyOfRange(str_list, i+1, str_list.length);
             }
-        }
-        if(index_l != -1){
-            answer = Arrays.copyOf(str_list, index_l);
-        }  
-        else if(index_r != -1){
-            answer = Arrays.copyOfRange(str_list, index_r+1, str_list.length);
-        }
+        } 
         return answer;
     }
 }
