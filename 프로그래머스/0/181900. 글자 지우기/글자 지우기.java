3d47a15b7b45ff018ee592ba51ed6464e7640c09
chaewon <150315730+cochae@users.noreply.github.com>
2025-09-25 14:15:19 +0900

    [level 0] Title: 글자 지우기, Time: 0.30 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder();
+        String[] arr = my_string.split("");
+        for(int i : indices){
+            arr[i] = "";
+        }
+        for(String s : arr) sb.append(s);
+        return sb.toString();
+    }
+}
